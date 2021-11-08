a6711bef009c46d8f498746c5052865337f54622
Andrey Shobonov <lebmed9@gmail.com>
2021-11-07 19:56:36 -0600

    homework_2

---
+++ homework_2.js
@@ -0,0 +1,47 @@
+console.log("кот" > "код");
+// true
+
+console.log("2" + 2 * "2");
+// 24
+
+console.log(undefined == null);
+// true
+
+console.log(undefined === null);
+// false
+
+console.log(null == 0);
+// true
+
+console.log(null - false + true);
+// 1
+
+console.log(1/"1");
+// 1
+
+console.log("2" * "3");
+// 6
+
+console.log(4 + 5 + "O");
+// 9O
+
+console.log("l" + 4 + 5);
+// 145
+
+console.log("4" - 2);
+// 2
+
+console.log("4" - "4x");
+// NaN
+
+console.log(null == false);
+// false
+
+console.log(" -4 "/ 0 + 1);
+// Infinity
+
+console.log(null + 1);
+// 1
+
+console.log(undefined + null);
+// NaN
