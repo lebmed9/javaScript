a128fff5625d1a3c26e4d13ba11d84e708b5c609
Andrey Shobonov <lebmed9@gmail.com>
2021-11-10 18:09:19 -0600

    06.11.2021

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-const a = 3;
-if (a) {
-    console.log('OK');
-}
+// const a = 5;
+// if (a === 3) {
+//     console.log('a === 3');
+// } else if (a === 4) {
+//     console.log('a ===4');
+// } else {
+//     console.log('чему же оно равно')
+// }
+
+// const a = 1;
+// const result = a > 3 ? "a  > 3" : "a <= 3";
+// console.log(result);
+
+// const a = 1;
+// if (a > 3) {
+//     result = "a > 3";
+// } else {
+//     result = "a <=3"
+// }
+// console.log(result);
