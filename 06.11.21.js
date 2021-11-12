2df4194019b52e23248439673e273b57b2244cf9
Andrey Shobonov <lebmed9@gmail.com>
2021-11-11 19:17:54 -0600

    06.11.2021

---
+++ 06.11.21.js
@@ -0,0 +1,82 @@
+///////////////////////////задачи 06.11.2021/////////////////////////////
+
+// let a = 34;
+// let b = 7;
+// let c = 6;
+// if (a > b && a > c) {                                 
+//     console.log('a');                    
+// } else if (b > a && b > c) {
+//     console.log('b');
+// } else {
+//     console.log('c');
+// }
+
+
+// let temp = "-33";
+// if (temp < -30) {
+//     console.log('Оставайтесь дома!');
+// }   else if(temp <= -10) { 
+//     console.log('Сегодня холодно');
+// }   else if (temp <= 5) {
+//     console.log('Не холодно'); 
+// }   else if (temp <= 15) {
+//     console.log('Тепло');
+// }   else if (temp <= 25) {
+//     console.log('Очень тепло');
+// }   else if (temp <= 35) {
+//     console.log('Жарко');
+// }   else {
+//     console.log('Пекло!');
+// }
+
+// const role = 'user';
+// switch (role) {
+//     case 'user' : {
+//         console.log("31 год");
+//         break;
+//     }
+//     case 'manager' : {
+//         console.log("35 лет");
+//         break;
+//     }
+//     case 'admin' : {
+//         console.log("37 лет");
+//         break;
+//     }
+//     default : {
+//         console.log("не знаю сколько лет");
+//     }
+// }
+
+
+// const b = 6;
+// if (b > 0 && b < 10 && b % 2 === 0) {
+//     console.log("Сработало!!!");
+// }
+
+// const b = 17;
+// if (!b) {
+//     console.log("Сработало!!!")
+// }
+// if (b !== 0) {
+//     console.log("Сработало!!!")
+// }
+
+//////////////////////////////////////////////////  while  /////////////////////////////////////////////
+
+// let i = 0;
+// while(i < 10) {
+//     console.log(i);
+//     i++;
+// }
+
+// let i = 0;
+// let j = 0;
+// while (i < 10) {
+//     console.log(i);
+//     i++;
+// while (j < 20) {
+//     console.log(j);
+//     j++;
+//     }   
+// }
--- index.js
+++ index.js
@@ -18,3 +18,151 @@
 //     result = "a <=3"
 // }
 // console.log(result);
+
+
+/////////////////////////////////////////////////06.11.2021///////////////////////////////////////////
+
+
+// const role = "user";
+// switch(role) {
+//     case 'user' : {
+//         console.log("разрешено в личный кабинет");
+//         break;
+//     }
+//     case 'manager': {
+//         console.log ("разрешен доступ добавленеи контента")
+//         break;
+//     }
+//     case 'admin': {
+//         console.log("разрешено в адинку")
+//         break;
+//     } 
+//     default: {
+//         console.log("не авторизован")
+//     }
+// }
+
+
+// const role = "user";
+// switch (role) {
+//     case 'user' : {
+//         console.log("разрешено в личный кабинет");
+//         break;
+//     }
+//     case 'manager':
+//     case 'admin': {
+//         console.log("разрешено в адинку")
+//         break;
+//     } 
+//     default: {
+//         console.log("не авторизован")
+//     }
+// }
+
+// const role = "user";
+// if (role === "user") {
+//     console.log("разрешен в личный кабинет");
+// }   else if(role === "manager") {
+//     console.log ("разрешен доступ добавленеи контента")
+// }   else if(role === "admin") {
+//     console.log("разрешено в адинку")
+// }   else {
+//     console.log("не авторизован")
+// }
+
+
+///////////////////////////задачи 06.11.2021/////////////////////////////
+
+// let a = 34;
+// let b = 7;
+// let c = 6;
+// if (a > b && a > c) {                                 
+//     console.log('a');                    
+// } else if (b > a && b > c) {
+//     console.log('b');
+// } else {
+//     console.log('c');
+// }
+
+
+// let temp = "-33";
+// if (temp < -30) {
+//     console.log('Оставайтесь дома!');
+// }   else if(temp <= -10) { 
+//     console.log('Сегодня холодно');
+// }   else if (temp <= 5) {
+//     console.log('Не холодно'); 
+// }   else if (temp <= 15) {
+//     console.log('Тепло');
+// }   else if (temp <= 25) {
+//     console.log('Очень тепло');
+// }   else if (temp <= 35) {
+//     console.log('Жарко');
+// }   else {
+//     console.log('Пекло!');
+// }
+
+// const role = 'user';
+// switch (role) {
+//     case 'user' : {
+//         console.log("31 год");
+//         break;
+//     }
+//     case 'manager' : {
+//         console.log("35 лет");
+//         break;
+//     }
+//     case 'admin' : {
+//         console.log("37 лет");
+//         break;
+//     }
+//     default : {
+//         console.log("не знаю сколько лет");
+//     }
+// }
+
+
+// const b = 6;
+// if (b > 0 && b < 10 && b % 2 === 0) {
+//     console.log("Сработало!!!");
+// }
+
+// const b = 17;
+// if (!b) {
+//     console.log("Сработало!!!")
+// }
+// if (b !== 0) {
+//     console.log("Сработало!!!")
+// }
+
+//////////////////////////////////////////////////  while  /////////////////////////////////////////////
+
+// let i = 0;
+// while(i < 10) {
+//     console.log(i);
+//     i++;
+// }
+
+// let i = 0;
+// let j = 0;
+// while (i < 10) {
+//     console.log(i);
+//     i++;
+// while (j < 20) {
+//     console.log(j);
+//     j++;
+//     }   
+// }
+
+// let i = 11;
+// let j = 0;
+// do {
+//     console.log(i);
+//     i++;
+// }   while (i < 10);
+
+/////////////////////////////////  for  /////////////////////////////////////
+
+for (let i = 0; i < 10; i++) {
+    console.log("for", i);
+}
