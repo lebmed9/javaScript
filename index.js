0de7974f10818c1f970e940511442a6050d196a2
Andrey Shobonov <lebmed9@gmail.com>
2022-01-19 18:43:44 -0600

    5

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+// function example () {
+// 	let closure  = 100;
+// 	return function (b) {
+// 		return closure + b;
+// }
+// }
+// console.log(example(100));
+
+// const closureExample = example();
+
+// console.log(closureExample(100)); // 200
