e0c758164975b642ad80369f3e504bc6071bacab
Andrey Shobonov <lebmed9@gmail.com>
2022-01-20 11:28:53 -0600

    07

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
+// Реализовать объект пользователя с полями name, lastname ,middleName, birthDate. Реализовать геттер/сеттер fullname,
+// который возвращает одной строкой полное имя пользователя и записывает имя, фамилию и отчество при присвоении значения.
+// Реализовать геттер age, который возвращает возраст используя данные даты рождения.
 
-
-
-
-
-// function example () {
-// 	let closure  = 100;
-// 	return function (b) {
-// 		return closure + b;
-// }
-// }
-// console.log(example(100));
-
-// const closureExample = example();
-
-// console.log(closureExample(100)); // 200
+const user = {
+  name: "Иван",
+  lastName: "Иванов",
+  middleName: "Иванович",
+  birthDate: "21.08.1984",
+  get fullName() {
+    return `${this.name} ${this.lastName} ${this.middleName}`;
+  },
+  set fullName(value) {
+    return;
+  },
+};
+console.log(user.fullName);
